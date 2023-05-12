38aaf5dc0b5e20e0d9bac896b3c17236c1929f8b
Sirawan Eanthaisong <gnotsfw@gmail.com>
2023-05-12 09:20:33 +0700

    feature :spaarkles: add mul

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ import "fmt"
 // 3 push
 
 func sum(a, b int) int {
-	return a + b 
+	return a + b
+}
+
+func mul(a, b int) int {
+	return a * b
 }
 
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println(sum(5, 3))
+	fmt.Println(mul(5, 3))
 }
-
