7e6e6b45c6e131b00c5ca19f6777536fda194f5a
Sirawan Eanthaisong <gnotsfw@gmail.com>
2023-05-12 09:18:13 +0700

    feature :sparkles: add sub func

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import "fmt"
 // 2 commit
 // 3 push
 
+func sub(a, b int) int {
+	return a - b
+}
+
 func sum(a, b int) int {
 	return a + b 
 }
@@ -13,5 +17,6 @@ func sum(a, b int) int {
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println(sum(5, 3))
+	fmt.Println(sub(5, 3))
 }
 
